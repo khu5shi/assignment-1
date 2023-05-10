5f7e8c1b9fe3fe327a8fdce446fb4484ac1e5dab
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-11 01:30:29 +0530

    Create question-2.cpp

---
+++ question-2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,a,c;
+    cout<<"input radius of circle";
+    cin>>r;
+    a=3.14*r*r;
+    c=2*3.14*r;
+    cout<<"area of circle="<<a<<endl;
+    cout<<"circumference of circle="<<c ;
+    return 0;
+}
