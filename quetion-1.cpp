052902269e4235c7248219285002cad7fd2f1085
khu5shi <132192823+khu5shi@users.noreply.github.com>
2023-05-11 01:19:58 +0530

    Create quetion-1.cpp

---
+++ quetion-1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,s,per;
+    cout<<"input 5 subject marks";
+    cin>>a>>b>>c>>d>>e;
+   
+   s=a+b+c+d+e;
+     
+    
+     per=s/5;
+     cout<<s<<endl;
+     cout<<per;
+     
+     return 0;
+
+}
