72951ae82751277502f6bcc01e17ec14a52a8490
i am khushi <132192823+khu5shi@users.noreply.github.com>
2023-05-18 18:52:10 +0530

    Create question-5.cpp

---
+++ question-5.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"input number you want to swap";
+     cin>>a>>b;
+     c=a;
+     a=b;
+     b=c;
+     cout<<"swaped number are"<<a<<b;
+     
+     return 0;
+
+}
