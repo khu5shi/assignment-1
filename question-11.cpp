7e78f17176d5d6051a9dc53dae69aabd08402996
i am khushi <132192823+khu5shi@users.noreply.github.com>
2023-05-18 22:10:17 +0530

    Create question-11.cpp

---
+++ question-11.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b=0;
+    cout<<"input the numbers you want to sum";
+     cin>>a;
+     for(int i=1;i<=a;i++)
+     {
+        b=b+i;
+     }
+     cout<<"the sum of terms is"<<endl<<b;
+     
+     return 0;
+}
