22b1642529a5c1bed5d24f5123538ea8ea12c8f9
i am khushi <132192823+khu5shi@users.noreply.github.com>
2023-05-18 18:56:53 +0530

    Create question-6.cpp

---
+++ question-6.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"input number you want to check equal or not";
+     cin>>a>>b;
+     if(a==b)
+     {
+        cout<<"numbers are equal";
+     }
+     else
+     {
+        cout<<"numbers are not equal";
+     }
+     
+        
+     
+     return 0;
+
+}
