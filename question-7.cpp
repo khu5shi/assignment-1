c051b0b565bbe8b4b97a06339184b48c64ca9f23
i am khushi <132192823+khu5shi@users.noreply.github.com>
2023-05-18 19:01:33 +0530

    Create question-7.cpp

---
+++ question-7.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"input number you want to check even or odd";
+     cin>>a;
+     if(a%2==0)
+     {
+        cout<<"numbers is even";
+     }
+     else
+     {
+        cout<<"numbers is odd";
+     }
+     
+        
+     
+     return 0;
+
+}
