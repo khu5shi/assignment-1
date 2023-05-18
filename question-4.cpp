3a9951b08f47684858a8032d16d46c20ecd5a522
i am khushi <132192823+khu5shi@users.noreply.github.com>
2023-05-18 07:51:02 +0530

    Create question-4.cpp

---
+++ question-4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"input temperature in centigrade ";
+
+    cin>>a;
+    b=(9*a+160)/5;
+    cout<<"temperature in fahrenhiet "<<b;
+
+     
+     return 0;
+
+}
